3d6cc0478cf27f283d0f746196f3c36c3eaf687c
Vasiliy Yazadji <vyazadji@rocketfuel.com>
2017-01-26 01:07:58 +0200

    Fix name issue. Rename lib folder

--- MMM-FullCalendar.js
+++ MMM-FullCalendar.js
@@ -8,14 +8,14 @@ Module.register("MMM-FullCalendar",{
     return [
       'moment.js', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
       'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
-      this.file('/fullCalendar/fullCalendar.js'),
-      this.file('/fullCalendar/gcal.js'),
+      this.file('lib/fullcalendar.js'),
+      this.file('lib/gcal.js'),
     ];
   },
 
   getStyles: function() {
     return [
-      this.file('fullCalendar/fullCalendar.css'), // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+      this.file('lib/fullCalendar.css'), // will try to load it from the vendor folder, otherwise it will load is from the module folder.
     ];
   },
 
